a3c8e1240901a923f4615f5e9205a1becb0dc1a8
Ewelina Sobora <e@e-dot.uk>
2023-06-19 15:07:10 +0100

    formatted signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  skip_before_action :verify_authenticity_token
+  # skip_before_action :verify_authenticity_token
   # protect_from_forgery with: :reset_session, prepend: true
   before_action :authenticate_user!
 end
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,10 +2,19 @@
 //= require_tree .
 
 import "@hotwired/turbo-rails"
+import { Application } from "@hotwired/stimulus"
 import "./controllers"
 import * as bootstrap from "bootstrap"
+
+const application = Application.start()
 
 let popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
 let popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
   return new bootstrap.Popover(popoverTriggerEl)
 })
+
+// Configure Stimulus development experience
+application.debug = false
+window.Stimulus = application
+
+export { application }
