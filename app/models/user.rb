efde7754bc7fa131852ac0a24e75fd787e68e169
Ewelina Sobora <e@e-dot.uk>
2023-07-11 19:58:29 +0100

    Validate email format

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :confirmable
 
   has_many :events, dependent: :delete_all
+
+  validates :email, format: { with: /\A[A-Z0-9._%a-z-]+@e-dot\.uk|.pl\z/, message: 'must be a e-dot account' }
 end
